95a0a13dd7c20c899fc56eefab6d3565c321323e
MD-SelimReza <rezaselim405@gmail.com>
2025-01-30 10:59:56 +0600

    Fix width issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import ReviewsSection from './components/RiviewSection';
 
 function App() {
   return (
-    <div className="container mx-auto p-6 bg-white">
+    <div className="container max-w-7xl mx-auto p-6 bg-white">
       <ProfileCard />
       <ServiceCard />
       <ReviewsSection />
